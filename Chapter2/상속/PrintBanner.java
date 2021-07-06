fafcba8fb0e03b64713394c1aaea01d812b8c3bc
kimsy08220 <kimsy08220@naver.com>
2021-07-07 00:15:21 +0900

    Adapter

---
+++ Chapter2/상속/PrintBanner.java
@@ -0,0 +1,13 @@
+package »ó¼Ó;
+
+public class PrintBanner extends Banner implements Print{
+	public PrintBanner(String string) {
+		super(string);
+	}
+	public void printWeak() {
+		showWithParen();
+	}
+	public void printStrong() {
+		showWithAster();
+	}
+}
