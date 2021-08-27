0be24a0d80de2a9573d96ccc2e3d7949771e3db0
kimsy08220 <kimsy08220@naver.com>
2021-08-27 23:39:00 +0900

    Chain of responsibility

---
+++ Chapter14/practice4/NoSupport.java
@@ -0,0 +1,10 @@
+package practice4;
+
+public class NoSupport extends Support {
+	public NoSupport(String name) {
+		super(name);
+	}
+	protected boolean resolve(Trouble trouble) {
+		return false;
+	}
+}
