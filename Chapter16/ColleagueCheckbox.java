4bcc54db97d0bd27593d8ee0f26f876b699daff4
kimsy08220 <kimsy08220@naver.com>
2021-08-29 03:21:43 +0900

    Mediator

---
+++ Chapter16/ColleagueCheckbox.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class ColleagueCheckbox extends Checkbox implements ItemListener, Colleague {
+	private Mediator mediator;
+	public ColleagueCheckbox(String caption, CheckboxGroup group, boolean state) {
+		super(caption, group, state);
+	}
+	public void setMediator(Mediator mediator) {
+		this.mediator = mediator;
+	}
+	public void setColleagueEnabled(boolean enabled) {
+		setEnabled(enabled);
+	}
+	public void itemStateChanged(ItemEvent e) {		// ItemListener
+		mediator.colleagueChanged();
+		// 상태가 변하면 Mediator에게 통지
+	}
+}
---
+++ Chapter16/practice1/ColleagueTextField.java
@@ -0,0 +1,23 @@
+package practice1;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class ColleagueTextField extends TextField implements TextListener, Colleague {
+	private Mediator mediator;
+	public ColleagueTextField(String text, int columns) {
+		super(text, columns);
+	}
+	public void setMediator(Mediator mediator) {
+		this.mediator = mediator;
+	}
+	public void setColleagueEnabled(boolean enabled) {
+		setEnabled(enabled);
+		// enabled가 true이면 배경색 흰색, enabled가 false이면 배경색 회색
+		setBackground(enabled ? Color.white : Color.lightGray);
+	}
+	public void textValueChanged(TextEvent e) {		// TextListener 메소드
+		mediator.colleagueChanged();
+		// 문자열이 변하면 Mediator에게 통지
+	}
+}
