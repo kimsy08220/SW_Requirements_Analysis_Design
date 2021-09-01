02826ac0a0907a0d605f911c663ec9e9ba62385e
kimsy08220 <kimsy08220@naver.com>
2021-09-02 00:10:40 +0900

    Flyweight

---
+++ Chapter20/BigChar.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class BigChar {
+	private char charname;
+	private String fontdata;
+	
+	public BigChar(char charname) {
+		this.charname = charname;
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("big" + charname + ".txt"));
+			String line;
+			StringBuffer buf = new StringBuffer();
+			while ((line = reader.readLine()) != null) {
+				buf.append(line);
+				buf.append("\n");
+			}
+			reader.close();
+			this.fontdata = buf.toString();
+		} catch (IOException e) {
+			this.fontdata = charname + "?";
+		}
+	}
+	public void print() {
+		System.out.println(fontdata);
+	}
+}
---
+++ Chapter20/practice2/BigString.java
@@ -0,0 +1,28 @@
+package practice2;
+
+public class BigString {
+	private BigChar bigchars[];
+	
+	public BigString(String string, Boolean shared) {
+		if (shared)
+			initShared(string);
+		else
+			initUnShared(string);
+		
+	}
+	private void initShared(String string) {
+		bigchars = new BigChar[string.length()];
+		BigCharFactory factory = BigCharFactory.getInstance();
+		for (int i = 0; i < bigchars.length; i++)
+			bigchars[i] = factory.getBigChar(string.charAt(i));		// BigChar °´Ã¼ °øÀ¯
+	}
+	private void initUnShared(String string) {
+		bigchars = new BigChar[string.length()];
+		for (int i = 0; i < bigchars.length; i++)
+			bigchars[i] = new BigChar(string.charAt(i));		
+	}
+	public void print() {
+		for (int i = 0; i < bigchars.length; i++)
+			bigchars[i].print();
+	}
+}
