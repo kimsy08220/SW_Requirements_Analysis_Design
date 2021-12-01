372670ba253acf968cca1ff145c281481912a98a
kimsy08220 <kimsy08220@naver.com>
2021-12-02 02:03:21 +0900

    review

---
+++ review/Chapter12.Decorator/practice1/Display.java
@@ -0,0 +1,12 @@
+package practice1;
+
+public abstract class Display {
+	public abstract int getColumns();
+	public abstract int getRows();
+	public abstract String getRowText(int row);
+	// Template Method ¿ªÇÒ
+	public final void show() {
+		for (int i = 0; i < getRows(); i++)
+			System.out.println(getRowText(i));
+	}
+}
---
+++ review/Chapter12.Decorator/practice1/Main.java
@@ -0,0 +1,17 @@
+package practice1;
+
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Display b1 = new StringDisplay("Hello, world");
+		Display b2 = new UpdownBorder(b1, '-');
+		Display b3 = new SideBorder(b2, '*');
+		b1.show();
+		b2.show();
+		b3.show();
+		
+		Display b4 = new FullBorder(new UpdownBorder(new SideBorder(new UpdownBorder(new SideBorder(new StringDisplay("æ»≥Á«œººø‰"), '*'), '='), '|'), '/'));
+		b4.show();
+	}
+}
