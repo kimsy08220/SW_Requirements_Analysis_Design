ed12d75a62f8769cb4155b427a8976857505cd70
kimsy08220 <kimsy08220@naver.com>
2021-12-04 01:08:43 +0900

    review

---
+++ review/Chapter14.Chain of Responsibility/Trouble.java
@@ -0,0 +1,13 @@
+
+public class Trouble {
+	private int number;
+	public Trouble(int number) {
+		this.number = number;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public String toString() {
+		return "[Trouble " + number + "]";
+	}
+}
